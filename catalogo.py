f9d1a396148d56db9aeae7b887e6b0edd10bae53
Axel Holmes <41641300+axeltupac@users.noreply.github.com>
2021-06-04 10:21:44 -0300

    Update catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -2,33 +2,31 @@ import requests
 import json
 
 
-def get_pelis_ids():
+def get_pelis():
     url = "https://playdata.starz.com/metadata-service/play/partner/Web_ES/v8/blocks?playContents=map&lang=es-" \
           "ES&pages=MOVIES&includes=title,releaseYear,logLine,directors,genres,actors,runtime"
-    try:
-        response = requests.get(url)
-    except:
-        print('An error has occurred: ')
+   
+    response = requests.get(url)
+   
     pelis = response.json()
     pelis = pelis['blocks'][-1]['playContentsById']
     return pelis
-def get_series_ids():
+def get_series():
     url_series="https://playdata.starz.com/metadata-service/play/partner/Web_ES/v8/blocks?playContents=map&lang=es-ES&pages=SERIES&includes=title,releaseYear,logLine,directors,genres,actors,runtime"
-    try:
-        response= requests.get(url_series)
-    except:
-        print('An error has ocurred: ')
+   
+    response= requests.get(url_series)
+    
     series= response.json()
     series= series['blocks'][-1]['playContentsById']
     return series
     
     
-pelis = get_pelis_ids()
+pelis = get_pelis()
 peli= open('pelis_info.txt', 'w')
 peli.write(json.dumps(pelis, indent=4))
 peli.close()
 
-series=get_series_ids()
+series=get_series()
 serie=open('series_info.txt', 'w')
 serie.write(json.dumps(series, indent=4))
 serie.close()
